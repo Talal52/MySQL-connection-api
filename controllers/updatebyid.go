7a06a8fda9749950d1afc08fb67a093a0fc29533
talalafzal <talalafzal57@gmail.com>
2023-07-06 01:11:50 -0700

    get, getByID, post, UpdateByID

--- controllers/updatebyid.go
+++ controllers/updatebyid.go
@@ -19,7 +19,7 @@ func UpdateUserById(c *gin.Context) {
 		return
 	}
 	defer db.Close()
-
+	fmt.Println(body)
 	id := c.Param("id")
 
 	updateData := model.UserUpdate{}
@@ -31,75 +31,24 @@ func UpdateUserById(c *gin.Context) {
 
 	fmt.Println(updateData.Age, updateData.Email, updateData.Name)
 
-	rows, err := db.Query(`UPDATE * FROM users WHERE id = ?`, id)
+	result, err := db.Exec("UPDATE users SET name = ?, email = ?, age = ? WHERE id = ?", updateData.Name, updateData.Email, updateData.Age, id)
 	if err != nil {
 		fmt.Println("Failed to execute the query:", err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "database query error"})
 		return
 	}
-	defer rows.Close()
-
-	var users []model.UserUpdate
-
-	for rows.Next() {
-		var user model.UserUpdate
-		var createdAt string
-
-		err := rows.Scan(&user.Name, &user.Email, &user.Age, &createdAt)
-		if err != nil {
-			fmt.Println("Failed to scan row:", err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "database scan error"})
-			return
-		}
-		// parsedTime, err := time.Parse("2006-01-02 15:04:05", createdAt)
-		// if err != nil {
-		// 	fmt.Println("Failed to parse created_at:", err)
-		// 	c.JSON(http.StatusInternalServerError, gin.H{"error": "time parsing error"})
-		// 	return
-		// }
-		// user.CreatedAt = parsedTime
-		users = append(users, user)
-	}
-	if err := rows.Err(); err != nil {
-		fmt.Println("Error occurred while iterating over rows:", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "database row iteration error"})
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		fmt.Println("Failed to retrieve the number of affected rows:", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "database query error"})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"Record Updated": body})
+	if rowsAffected == 0 {
+		c.JSON(http.StatusAccepted, gin.H{"message": "user already updated"})
+		return
+	}
 
-	// 	fmt.Println("Failed to execute the query:", err)
-	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": "database query error"})
-	// 	return
-	// }
-	// defer rows.Close()
+	c.JSON(http.StatusOK, gin.H{"Record Updated": updateData})
 
-	// var users []model.User
-
-	// for rows.Next() {
-	// 	var user model.User
-	// 	var createdAt string
-
-	// 	err := rows.Scan(&user.ID, &user.Name, &user.Email, &user.Age, &createdAt)
-	// 	if err != nil {
-	// 		fmt.Println("Failed to scan row:", err)
-	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "database scan error"})
-	// 		return
-	// 	}
-	// 	parsedTime, err := time.Parse("2006-01-02 15:04:05", createdAt)
-	// 	if err != nil {
-	// 		fmt.Println("Failed to parse created_at:", err)
-	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "time parsing error"})
-	// 		return
-	// 	}
-	// 	user.CreatedAt = parsedTime
-	// 	users = append(users, user)
-	// }
-	// if err := rows.Err(); err != nil {
-	// 	fmt.Println("Error occurred while iterating over rows:", err)
-	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": "database row iteration error"})
-	// 	return
-	// }
-
-	// c.JSON(http.StatusOK, users)
 }
